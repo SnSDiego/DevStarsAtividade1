b530bc2c78efcfcec3b5b172e4750eb269714549
DiegoFontes <diegofontesdois@gmail.com>
2022-12-03 17:18:54 -0300

    Aula 1 soma simples

---
+++ Atividade1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Atividade1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Licao1
+            Console.WriteLine("1 - Faça um programa para ler dois valores inteiros, e depois mostrar na tela a\n" +
+                "soma desses números com uma mensagem explicativa, conforme exemplos.");
+
+            Console.WriteLine("Insira o numero de A");
+            int numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o numero de B");
+            Console.WriteLine();
+            int numero2 = int.Parse(Console.ReadLine());
+            int soma = numero1 + numero2;
+            Console.WriteLine("A soma é:" + soma);
+            Console.WriteLine();
+
+            #endregion
+
+        }
+    }
+}
